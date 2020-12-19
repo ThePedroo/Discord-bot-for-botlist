8800c653b59cd0cb7e79cd00dab2058181b081aa
Pedro20090 <73797168+Pedro20090@users.noreply.github.com>
2020-12-19 16:38:33 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
+
+client.on('message', async (message) => {
+ if(message.mentions.users.first() && message.mentions.users.first().id == client,user.id) {
+   message.channel.send(`Ola ${message.author}`)
+   }
+  })
+
+client.login(require('./config.json').TOKEN)
